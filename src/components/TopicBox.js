cc90dd21c47172af6ab9c8c0b15e38eb925403e6
Rashmi <rathnakumarrashmi2@gmail.com>
2024-02-17 22:32:55 +0530

    dyanamic componets

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './topicbox.css'
 
 export default function TopicBox() {
+
+  const topic = "My favorite food is burger"
+  const item = " and pizza"
+
   return (
     <div>
       <div className="topicBox">
-        <span className='text'>This is the topic</span>
+        <span className='text'>{topic}{item}</span>
       </div>
     </div>
   )
