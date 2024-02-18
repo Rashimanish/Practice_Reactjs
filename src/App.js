853e12e322f1f70088462d635862c0a561380c41
Rashmi <rathnakumarrashmi2@gmail.com>
2024-02-18 09:39:43 +0530

    useState counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 
 import './App.css';
+import { useState } from 'react';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
+
+  const [count,setCount] = useState(0);
+
+  const increment = ()=>{
+    setCount(count+1);
+  }
+  const decrement = ()=>{
+    setCount(count-1);
+  }
+
   return (
-    <div>
-      <h1>Static Content</h1>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}>
-        </Route>
-        <Route path='/About' element={<About/>}>
-        </Route>
-        <Route path='/Contact' element={<Contact/>}>
-        </Route>
-      </Routes>
-      </BrowserRouter>
+   <div>
+      <span className='title'>My counter</span>
+      <p className='subtitle'>The count is {count}</p>
+      <button onClick={decrement} className='button'>-</button>
+      <button onClick={increment} className='button'>+</button>
     </div>
+ 
   );
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import TopicBox from '../components/TopicBox'
 
-import { Link, useNavigate } from 'react-router-dom'
+
 
 
 
 export default function Home() {
-  const Navigate = useNavigate();
+
   return (
     <div>
         <h1>Home Page</h1>
-        <Link to='/About'>Go to About Page</Link><br/>
-        <Link to='/Contact'>Go to Contact Page</Link><br/>
-        <button onClick={()=> Navigate('/About')}>
-            About Page
-        </button>
+        
 
     </div>
   )
